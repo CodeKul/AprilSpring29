a68a64f74ec1093396f3f5fc83c9b6e4cb88180c
Vaibhav N <vn202207@nexttechvision.com>
2024-05-21 14:49:05 +0530

    @Query

--- src/main/java/com/codekul/aprilspring29/onetomany/controller/CountryController.java
+++ src/main/java/com/codekul/aprilspring29/onetomany/controller/CountryController.java
@@ -5,10 +5,9 @@ import com.codekul.aprilspring29.onetomany.entity.Country;
 import com.codekul.aprilspring29.onetomany.repository.CountryRepo;
 import com.codekul.aprilspring29.onetomany.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("country")
@@ -18,9 +17,21 @@ public class CountryController {
     @Autowired
     private CountryService countryService;
 
+    @Autowired
+    private CountryRepo countryRepo;
+
     @PostMapping("save")
     public String saveCountry(@RequestBody CountryRequestDto country) {
         countryService.saveCountry(country);
         return "country saved..";
+    }
+
+    @GetMapping("getCountry/{id}/{name}")
+    public List<Country> saveCountry(@PathVariable Long id, @PathVariable String name) {
+        return countryRepo.findByIdAndName(id, name);
+    }
+    @GetMapping("getFunctionData")
+    public Long getFunctionData(@RequestParam Long a,@RequestParam Long b) {
+        return countryRepo.getFunctionData(a,b);
     }
 }
--- src/main/java/com/codekul/aprilspring29/onetomany/repository/CountryRepo.java
+++ src/main/java/com/codekul/aprilspring29/onetomany/repository/CountryRepo.java
@@ -2,6 +2,16 @@ package com.codekul.aprilspring29.onetomany.repository;
 
 import com.codekul.aprilspring29.onetomany.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CountryRepo extends JpaRepository<Country,Long> {
+
+    @Query(value = "select * from country where id = ? and name = ?",nativeQuery = true)
+    List<Country> findByIdAndName(Long id,String name);
+
+    @Query(value = "select * from addTwoNumber(?1,?2)",nativeQuery = true)
+    Long getFunctionData(@Param("a") Long a,@Param("b")Long b);
 }
